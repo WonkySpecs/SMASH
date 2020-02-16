160caaf6fb349ca19888f4570c0a362845cdf0e3
Will Taylor <w1ll100@hotmail.co.uk>
2020-02-16 17:18:50 +0000

    Don't load textures unnecessarily

--- world.c
+++ world.c
@@ -9,18 +9,24 @@ bool isLava(int x, int y) {
 
 Map initMap() {
     Map map;
+    Texture2D lavaTex = LoadTexture("assets/lava_0.png");
+    Texture2D stoneTextures[8];
+    for (int i = 0; i < 8; i++) {
+        char *texName = (char *)malloc(50 * sizeof(char));
+        sprintf(texName,
+                "assets/tomb_%d_%s.png",
+                i % 4,
+                i > 4 ? "old" : "new");
+        stoneTextures[i] = LoadTexture(texName);
+        free(texName);
+    }
+
     for(int x = 0; x < MAP_WIDTH; x++) {
         for(int y = 0; y < MAP_HEIGHT; y++) {
             if (isLava(x, y)) {
-                map.tiles[x][y] = (Tile){LoadTexture("assets/lava_0.png")};
+                map.tiles[x][y] = (Tile){lavaTex};
             } else {
-                char *texName = (char *)malloc(50 * sizeof(char));
-                sprintf(texName,
-                        "assets/tomb_%d_%s.png",
-                        x % 4,
-                        y % 2 == 0 ? "old" : "new");
-                map.tiles[x][y] = (Tile){LoadTexture(texName)};
-                free(texName);
+                map.tiles[x][y] = (Tile){stoneTextures[GetRandomValue(0, 7)]};
             }
         }
     }
